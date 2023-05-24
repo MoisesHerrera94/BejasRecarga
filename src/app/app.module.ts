11786f756aadeeb0956fbcf6bf9286f5bf8adb93
moisesherrera <moyhr94@gmail.com>
2023-05-23 22:05:51 -0300

    Creando componente items y drinks para el menu de bebidas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemComponent } from './components/item/item.component';
+import { DrinksComponent } from './components/drinks/drinks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent,
+    DrinksComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/item/item.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit {
+
+  beers = [
+    {
+      bebida:"BELGIAM BITTER", 
+      descripcion:"AA 4.5% - IBU 30", 
+      detalle:"Estilo Belga, ligera seca, perfumada con bastante lúpulo.", 
+      precioPinta:200, 
+      precio500ml:500, 
+      precio1Lt:700, 
+      precio2Lt:1000
+    }, 
+    {
+      bebida:"PALE ALE", 
+      descripcion:"AA 5,5% - IBU 20", 
+      detalle:"Rubia con amargor medio, espuma liviana. Estilo Inglés, clásico.", 
+      precioPinta:200, 
+      precio500ml:500, 
+      precio1Lt:700, 
+      precio2Lt:1200
+    }
+  ];
+    
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
